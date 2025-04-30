e3bf7c1976171abc573be4ca30c372bcc232b50e
TencerDe <tsharma2907@gmail.com>
2025-04-30 22:13:50 +0530

    UI update and some bugs fixed

--- elara_e_commerce/elara_e_commerce/urls.py
+++ elara_e_commerce/elara_e_commerce/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls')),
+    path('', include('shop.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
--- elara_e_commerce/shop/models.py
+++ elara_e_commerce/shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -38,10 +39,11 @@ class Product(models.Model):
 
 
 class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Cart {self.id}"
+        return f"{self.user.username}'s Cart"
 
 
 class CartItem(models.Model):
